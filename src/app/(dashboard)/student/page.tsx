5cb494242df4f00521e2985877e79bc890cdec7c
Chepkirui91 <chepkiruidaphne91@gmail.com>
2024-09-10 16:45:08 +0300

    big-calendar

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,6 +1,37 @@
+"use client"
+
+import React, { useState } from 'react';
+import EventCalendar from "@/components/@page-components/calendar/EventCalendar";
+import Announcements from "@/components/@page-components/announcements/page";
+import BigCalendar from "@/components/@shared-components/BigCalendar/page";
+import "react-big-calendar/lib/css/react-big-calendar.css"
+
 const StudentPage = () => {
+    // Get current date
+    const today = new Date();
+    const currentDate = today.toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric' });
+
+    // State to handle tabs
+    const [activeTab, setActiveTab] = useState('Day');
+
     return (
-        <div className=''>Student PAge</div>
+        <div className='p-4 flex gap-4 flex-col xl:flex-row text-black '>
+            {/* LEFT */}
+            <div className='w-full xl:w-2/3 flex flex-col'>
+                <h1 className="font-semibold text-xl">Schedule (4A)</h1>
+                <BigCalendar />
+
+                {/* Display current date */}
+                <div>{currentDate}</div>
+
+            </div>
+
+            {/* RIGHT */}
+            <div className='w-full xl:w-1/3 flex flex-col gap-8'>
+                <EventCalendar />
+                <Announcements />
+            </div>
+        </div>
     )
 }
 
--- src/components/@page-components/Menu.tsx
+++ src/components/@page-components/Menu.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import {role} from "@/lib/common.data";
 
 const menuItems = [
     {
@@ -119,22 +120,26 @@ const menuItems = [
 const Menu = () => {
     return (
         <>
-            <div className="">
-                {menuItems.map((i) => (
-                    <div className="" key={i.title}>
-                        <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
-                        {i.items.map(item => (
-                            <Link href={item.href} key={item.label}
-                                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
-                                <Image src={item.icon} alt="" width={20} height={20}/>
-                                <span className="hidden lg:block">{item.label}</span>
-                            </Link>
-                        ))}
-                    </div>
-                ))}
-            </div>
-        </>
-    )
+        <div className="">
+            {menuItems.map((i) => (
+                <div className="" key={i.title}>
+                    <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
+                    {i.items.map(item => {
+                        if (item.visible.includes(role)) {
+                            return (
+                                <Link href={item.href} key={item.label}
+                                      className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight">
+                                    <Image src={item.icon} alt="" width={20} height={20}/>
+                                    <span className="hidden lg:block">{item.label}</span>
+                                </Link>
+                            )
+                        }
+                    })}
+        </div>
+        ))}
+        </div>
+</>
+)
 }
 
 export default Menu;
