2791301e2f3a004509e032a142456ec446b12dc6
Chepkirui91 <chepkiruidaphne91@gmail.com>
2024-09-17 16:52:54 +0300

    table search

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,6 +1,33 @@
-const TeacherListPage = () =>{
+import TableSearch from "@/components/@page-components/table-search/page";
+import Image from "next/image";
+import Pagination from "@/components/@shared-components/pagination/page";
+
+const TeacherListPage = () => {
     return (
-        <div className="text-black">Teachers</div>
+        <div className="text-black bg-white p-4 rounded-md m-4 flex-1 mt-0">
+            {/*TOP*/}
+            <div className="flex items-center justify-between">
+                <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
+                <div className="flex flex-col md:flex-row items-center gap-4 w-full md-w-auto">
+                    <TableSearch/>
+                </div>
+                <div className="flex items-center gap-4 self-end">
+                    <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                        <Image src="/filter.png " alt="" width={14} height={14}/>
+                    </button>
+                    <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                        <Image src="/sort.png " alt="" width={14} height={14}/>
+                    </button>
+                    <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                        <Image src="/plus.png " alt="" width={14} height={14}/>
+                    </button>
+                </div>
+            </div>
+            {/*LIST*/}
+            <div className=""></div>
+            {/*PAGINATION*/}
+                <Pagination />
+        </div>
     )
 }
 
